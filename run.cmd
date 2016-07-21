4f285fb673c8479ce979d5a7cfe9ffe652974886
Sitar Harel <sitar@sitarharel.com>
2016-07-21 00:08:53 -0700

    Added run and update scripts

---
+++ run.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+C:\processing-3.1.1\processing-java.exe --sketch="C:\Users\Sitar\Documents\LEDcontrol\LightControl" --run
---
+++ update.cmd
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd c:\Users\Sitar\Documents\LEDcontrol\
+git pull
+
