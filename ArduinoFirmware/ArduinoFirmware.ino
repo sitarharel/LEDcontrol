f0052065b97a174e04e60b7cc9d5792c67f19b56
Sitar Harel <sitarharel@users.noreply.github.com>
2016-08-04 10:40:25 -0700

    Fixed still prominent bug - using signed bytes

--- ArduinoFirmware/ArduinoFirmware.ino
+++ ArduinoFirmware/ArduinoFirmware.ino
@@ -12,18 +12,19 @@ void setup() {
 }
 
 void loop() {
-    byte input[3];
+    int8_t input[3];
 
   // check if data has been sent from the computer:
   if (Serial.available()) {
       // read the most recent byte (which will be from -128 to 127):
-    Serial.readBytes(input, 3);
+    Serial.readBytes((char*) input, 3);
     // set the brightness of the LEDs:
-    int offThreshold = -128;
-    if(input[0] > offThreshold && input[1] > offThreshold && input[2] > offThreshold){
-      analogWrite(RED, input[0] + 128);
-      analogWrite(GREEN, input[1] + 128);
-      analogWrite(BLUE, input[2] + 128);
-    }
+    int offThreshold = 0;
+    int r = ((int) input[0]) + 128;
+    int g = ((int) input[1]) + 128;
+    int b = ((int) input[2]) + 128;
+    analogWrite(RED, r > offThreshold ? r : 0);
+    analogWrite(GREEN, g > offThreshold ? g : 0);
+    analogWrite(BLUE, b > offThreshold ? b : 0);
   }
 }
